b3e9c01d45128731deb409be90a97cf243225257
TMSadique <110740296+TMSadique@users.noreply.github.com>
2023-03-24 20:23:26 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -10,6 +10,6 @@ tip_percent = tip/100
 tip_amount = bill * tip_percent
 total_bill = bill + tip_amount
 
-per_pax = "{:.2f}".format(round(total_bill / pax,2))
+per_pax = "{:.2f}".format(round(total_bill / pax,2)) #"{:.2f}".format( formats for two decimal with last as 0, e.x: 35.60
 
 print(f"Eash person should pay ${per_pax}")
