2557ef30fd2b4390a8d7332f7f6ea1c4130de416
TMSadique <110740296+TMSadique@users.noreply.github.com>
2023-03-24 20:17:27 +0530

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,12 +1,15 @@
 print("Welcome to the tip calculator!")
 
-bill = input("What was the total bill? $")
+bill = float(input("What was the total bill? $"))
 
-tip = input("What percentage tip would you like to give? 10, 12, or 15? ")
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
 
-pax = input("How many people to split the bill? ")
+pax = int(input("How many people to split the bill? "))
 
+tip_percent = tip/100
+tip_amount = bill * tip_percent
+total_bill = bill + tip_amount
 
-per_pax = (float(bill) / int(pax)) * ((100 + int(tip))/100)
+per_pax = total_bill / pax
 
 print(f"Eash person should pay ${round(per_pax,2)}")
